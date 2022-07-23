9bec054d590257bf5d4fde73bd72e6900a50b132
0sik <ghdtlrdudtlr@naver.com>
2022-07-23 21:19:22 +0900

    5

---
+++ study/cafe.sql
@@ -0,0 +1,22 @@
+ --주문#2에 담긴 음료들
+ --join문법: 다른 테이블을 붙여옴
+ SELECT
+    beverages.name
+FROM
+    order_details
+JOIN beverages ON
+    beverages.id = order_details.beverage_id
+WHERE
+    order_details.order_id=2 
+;    
+--주문#14의 매출
+--JOIN 문법 사용
+SELECT
+    SUM(order_details.count * beverages.price)
+FROM
+    order_details
+Join beverages ON
+    beverages.id = order_details.beverage_id
+WHERE
+    order_id=14
+;   
